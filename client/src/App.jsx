f8ac82880172218c136c636a9b8e146e8c7cbcda
Danilashionshion <danilofernandesdasilva29@gmail.com>
2024-06-18 16:09:59 -0300

    New commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './index.css'
+import './componentes/Navbar.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/componentes/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <>
+            <navbar classname="border">
+                <h1> Ola </h1>
+            </navbar>
+        </>
+    )
+}
+
+export default Navbar
