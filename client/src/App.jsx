b3361ab960cfeae3fa1ed72aec7713071d468340
Danilashionshion <danilofernandesdasilva29@gmail.com>
2024-07-02 16:07:14 -0300

    for vscode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,13 @@ import './index.css'
 import MenuAppBar from './componentes/Navbar.jsx'
 import  SimpleContainer from './componentes/Textbox.jsx'
 import { Container } from '@mui/material'
+import axios from 'axios'
 
 function App() {
   
-
+  const api = axios.create({
+    baseURL:('http//:localhost:3240')
+  })
   return (
     
     <>
