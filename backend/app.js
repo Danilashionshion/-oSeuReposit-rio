c720a6025cf0321953829dcca9510bf5b6ef42ba
Danilashionshion <danilofernandesdasilva29@gmail.com>
2024-07-02 14:39:41 -0300

    backend start

---
+++ backend/app.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import register from './connection.js';
+import bcrypt from 'bcryptjs'
+
+const app=express();
+const PORT=3240
+
+
+app.use(express.json())
+
+app.get("/",async(req,res)=>{
+const src=await register.find();
+res.send(src)
+});
+
+app.post("/",async(req,res)=>{
+
+const verification=await register.findOne({email:req.body.email});
+
+if(verification) return res.status(400).send("Email já Existente!")
+    const cad=new register({
+        nome:req.body.nome,
+        cpf:req.body.cpf,
+        email:req.body.email,
+        senha:bcrypt.hashSync(req.body.senha)
+    });
+    
+    try {
+        const sucess=cad.save()
+        res.send("Sucesso!")
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("ERRO")
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log("Server Running!.")
+})
